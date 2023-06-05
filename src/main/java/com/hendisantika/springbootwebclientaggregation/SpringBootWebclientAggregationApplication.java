c9bcf0cb209f11cecaf5b7f04ce31612e92f493d
hendisantika <hendisantika@yahoo.co.id>
2023-06-05 16:39:09 +0700

    #1 - Initial commit ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootwebclientaggregation/SpringBootWebclientAggregationApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebclientaggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebclientAggregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebclientAggregationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootwebclientaggregation/SpringBootWebclientAggregationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebclientaggregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebclientAggregationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
